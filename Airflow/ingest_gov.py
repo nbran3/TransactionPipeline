b1584947b2cc0841c4cef98e98909d18c63aa61b
nbran3 <nbwan3@gmail.com>
2025-07-20 11:30:05 -0400

    added ingestion script for gov data

---
+++ Airflow/ingest_gov.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+bucket_name = 'transactionsnbran'
+local_folder = './data/gov/'
+s3 = boto3.client('s3')
+
+def upload_gov_files() -> None:
+    for filename in os.listdir(local_folder):
+        local_path = os.path.join(local_folder, filename)
+
+        if filename.endswith('.csv'):
+            s3_key = f"data/csv/{filename}"
+        elif filename.endswith('.json'):
+            s3_key = f"data/json/{filename}"
+        else:
+            print(f"Skipped (unsupported format): {filename}")
+            continue
+        try:
+            s3.upload_file(local_path, bucket_name, s3_key)
+            print(f"{filename} uploaded to s3://{bucket_name}/{s3_key}")
+            os.remove(filename)
+        except Exception as e:
+            print(f"Failed to upload {filename}: {e}")
+
